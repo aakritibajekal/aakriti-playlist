dfd911419f7d75ea68e9218ce8b5033664d82e28
Aakriti Bajekal <aakritibajekal@Aakritis-MacBook-Pro.local>
2020-04-18 12:18:26 -0600

    like and delete

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Aakriti's Playlist</h1>
+    <Playlist />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Playlist from './components/playlist.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   Playlist
   }
 }
 </script>
---
+++ src/components/song.vue
@@ -0,0 +1,33 @@
+<template>
+<li v-if="!song.played">
+    {{ song.name + " by " + song.artist }}
+    <button class="unliked-button" v-if="!song.favorite" @click="likeSong( song )">Add to Favourites</button>
+    <button class="liked-button" v-else @click="likeSong( song )">Love this song!</button>
+    <button v-on:click="removeSong( song )">Bored of this song</button> 
+  </li>
+</template>
+
+<script>
+export default {
+  name: "Song",
+  props: ["song"], 
+  methods: {
+    playedSong(song) {
+      this.$emit("played-song", song);
+    },
+    removeSong(song) {
+    this.$emit("delete-song", song);
+  },
+    likeSong(song) {
+      this.$emit("like-song", song);
+    }
+  }
+};
+</script>
+
+<style>
+.liked-button {
+ color: lightblue;
+ font: bold;
+}
+</style>
